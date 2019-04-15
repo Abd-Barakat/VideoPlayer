e27e990d5f4a3699c5a497800cfdfd26ed2dd44f
abd barakat <abdbarakat050@gmail.com>
2019-04-15 14:05:31 +0300

    Add project files.

---
+++ VedioPlayer/VideoMain.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace VideoPlayer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.VideoPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.VideoPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // VideoPlayer
+            // 
+            this.VideoPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.VideoPlayer.Enabled = true;
+            this.VideoPlayer.Location = new System.Drawing.Point(0, 0);
+            this.VideoPlayer.Margin = new System.Windows.Forms.Padding(0);
+            this.VideoPlayer.Name = "VideoPlayer";
+            this.VideoPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("VideoPlayer.OcxState")));
+            this.VideoPlayer.Size = new System.Drawing.Size(568, 398);
+            this.VideoPlayer.TabIndex = 0;
+            this.VideoPlayer.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.MediaPlayer_PlayStateChange);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(568, 398);
+            this.Controls.Add(this.VideoPlayer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(584, 437);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Video Player";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.VideoPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer VideoPlayer;
+    }
+}
+
---
+++ VedioPlayer/VideoMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.Devices;
+using System.Runtime.InteropServices;
+namespace VideoPlayer
+{
+
+
+    public partial class Main : Form
+    {
+
+
+        private string [] Videos;
+        private WMPLib.IWMPPlaylist PlayList;
+      
+        public Main()
+        {
+            InitializeComponent();
+            VideoPlayer.settings.playCount = int.MaxValue;
+            VideoPlayer.settings.autoStart = true;
+            PlayList = VideoPlayer.playlistCollection.newPlaylist("VideosList");
+        }
+
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            string currentDirectory = System.IO.Directory.GetParent(@"..\..\").FullName;
+            Videos = System.IO.Directory.GetFiles(currentDirectory + @"\Video");
+            for (int  Num_Videos =0; Num_Videos<Videos.Length; Num_Videos++)
+            {
+                var media = VideoPlayer.newMedia(Videos[Num_Videos]);
+                PlayList.appendItem(media);
+            }
+            VideoPlayer.currentPlaylist = PlayList;
+        }
+        private void MediaPlayer_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+
+            if (e.newState == 10) // Ready State 
+            {
+                VideoPlayer.Ctlcontrols.play();
+            }
+
+        }
+
+    }
+}
